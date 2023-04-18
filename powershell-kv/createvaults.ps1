9ccb68ae6b26bd5bd7f335df2ca818d2cc61a576
killerdaemon01 <bladeztrail007@gmail.com>
2023-04-18 18:27:22 +0530

    powershell scripts

--- powershell-kv/createvaults.ps1
+++ powershell-kv/createvaults.ps1
@@ -4,12 +4,14 @@ $keyVaults = Get-Content -Raw -Path .\createkeyvaults.json | ConvertFrom-Json | 
 $rggroup = "my_resource_group"
 $location = "eastus"
 $serviceprincipal = "850f2186-cf3d-413f-990c-95e0de63e3a2"
+$userprincipal = "AdminA@bladeztrail007gmail.onmicrosoft.com"
 
 # Loop through each key vault
 foreach ($keyVault in $keyVaults) {
     # Create the key vault
     New-AzKeyVault -VaultName $keyVault.name -ResourceGroupName $rggroup -Location $location
     Set-AzKeyVaultAccessPolicy -VaultName $keyVault.name -ServicePrincipalName $serviceprincipal -PermissionsToSecrets all
+    Set-AzKeyVaultAccessPolicy -VaultName $keyVault.name -UserPrincipalName $userprincipal -PermissionsToSecrets all
 
     # Loop through each secret in the key vault
     foreach ($secret in $keyVault.secrets) {
